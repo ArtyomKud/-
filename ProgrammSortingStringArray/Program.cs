a03faf1d8c78a01d0e18266b6e0ab90f1eb8fe81
Artyom <artyom_kudryavcev_91@mail.ru>
2023-01-07 19:46:40 +0700

    Добавил код по сортировке массива строк в программу ProgrammSortingStringArray

--- ProgrammSortingStringArray/Program.cs
+++ ProgrammSortingStringArray/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите текст");
+string userText = Console.ReadLine();
+
+
+string [] userStringArray = userText.Split(' ');
+int size = CounterStringLengthMax3Char(userStringArray);
+string [] resultStringArray = new string [size];
+SortingStringLengthMax3Char(userStringArray, resultStringArray);
+
+foreach (var sub in resultStringArray)
+{
+    Console.WriteLine($"{sub}");
+} 
+
+
+int CounterStringLengthMax3Char(string [] inputArray)
+{
+    int count = 0;
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+        if(inputArray[i].Length < 4)
+        {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+void SortingStringLengthMax3Char (string [] inputArray1, string [] inputArray2)
+{
+    for(int i = 0, j = 0; i < inputArray1.Length; i++)
+    {
+        if(inputArray1[i].Length < 4)
+        {
+            inputArray2[j] = inputArray1[i];
+            j++;
+        }
+    }
+
+}
